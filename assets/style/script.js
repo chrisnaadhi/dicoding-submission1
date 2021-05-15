3c0c56556048c25f6e340ed8b16bf7147f14eabe
Chrisna Adhi Pranoto <chrisnaadhip@gmail.com>
2021-05-15 20:14:54 +0700

    fix featured list etc.

--- assets/style/script.js
+++ assets/style/script.js
@@ -1,5 +1,5 @@
 function getClassification() {
-    let getData = document.querySelector("#book-form");
+    // let getData = document.querySelector("#book-form");
     let getTitle = document.querySelector("#title");
     let getSubject = document.querySelector("#subject");
     let getAuthor = document.querySelector("#author")
@@ -14,13 +14,6 @@ function getClassification() {
     document.querySelector("#result").innerHTML = class_number;
 };
 
-// function copyNumber() {
-//     let copyClassification = document.querySelector("#result");
-//     let textArea = document.createElement("textarea");
-//     textArea.value = copyClassification.textContent;
-//     document.body.appendChild(textArea);
-//     textArea.select();
-//     document.execCommand("Copy");
-//     textArea.remove();
-//     document.querySelector(".copied").innerHTML = "Sucessfully Copied :" + copyClassification.value;
-//   }
+function dropDown() {
+    document.querySelector("#dropdown").classList.toggle("show");
+}
